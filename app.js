4756703b1cfe57add63835015a4b7c9de87946ef
pabloarcones <pablo.arcones.castrillo@alumnos.upm.es>
2015-05-01 13:04:35 +0200

    esqueleto express-generator
    Versión completa de Quiz 1

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
-var express = require('express');
+var express = require('express');                             // Importar paquetes con middlewares
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
+var routes = require('./routes/index');                       // Importar enroutadores
 var users = require('./routes/users');
 
-var app = express();
+var app = express();                                          // Crear aplicacion
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'));              // Instalar generador de visitas EJS
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
-app.use(bodyParser.json());
+app.use(bodyParser.json());                                   // Instalar middlewares
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/', routes);                                         // Instalar enroutadores
+app.use('/users', users);                                     // Asociar rutas a sus gestores
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
+    var err = new Error('Not Found');                         // Resto de rutas: genera error 404 de HTTP
     err.status = 404;
     next(err);
 });
@@ -36,7 +36,7 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
+if (app.get('env') === 'development') {                       // Gestion de errores durante el desarrollo
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
@@ -48,7 +48,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res, next) {                       // Gestion de errores de produccion
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
@@ -57,4 +57,4 @@ app.use(function(err, req, res, next) {
 });
 
 
-module.exports = app;
+module.exports = app;                                         // Exportar app para comando de arranque
